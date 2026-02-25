7aa3267085fae82ab45e019892dac6ef6282b424
ar1kito <sabaabashidze714@gmail.com>
2026-02-25 20:34:36 +0400

    js-5

---
+++ js-5/j-script.js
@@ -0,0 +1,64 @@
+// 1
+// function dabewdva(){
+//     console.log("hello js");
+// }
+// dabewdva()
+
+// 2
+// function random(){
+//     return Math.round(Math.random()*100)
+// }
+// console.log(random());
+
+// 3
+// function DabewHtml(name){
+//     document.writeln(`gamarjoba ${name}`)
+// }
+// DabewHtml(prompt("shemoitanet saxeli"))
+
+// 4
+// function info(){
+//     alert("web page will save your data")
+//     document.writeln("welcome");
+// }
+// info()
+
+// 5
+// function ArrNum(arr=[],num=0){
+//     let arr1 = []
+//     for(let i =0;i<arr.length;i++){
+//         if(arr[i] > num) {
+//             arr1.push(arr[i])
+//         }
+//     }
+//     return arr1
+// }
+// let smh = [2,3,4,5,324,234,234,325,3,12,216,0]
+// console.log(ArrNum(smh,5))
+
+// 6
+// function ChekString(email){
+//     return email.includes("@")
+// }
+// console.log(ChekString("asqwdqdqwdqwdqwd"));
+// console.log(ChekString("asqw@dqdqwdqwdqwd"));
+// console.log(ChekString("asqwdqdqwdq@wdqwd"));
+// console.log(ChekString("asqwdqdqwdqwdqwd"));
+
+// 7
+// let Arr2D = [[2,5,6,8,10],[-11,11,111,1111],[213,123,23,42,1,2,0]]
+// for(let i =0;i<Arr2D.length;i++){
+//     for(let j =0;j<Arr2D[i].length;j++){
+//         if(Arr2D[i][j] > 10){
+//             console.log(Arr2D[i][j]);
+//         }
+//     }
+// }
+
+// 8
+// let Arr2D = [[2,5,6,8,10],[-11,11,111,1111],[213,123,23,42,1,2,0]]
+// for(let i =0;i<Arr2D.length;i++){
+//     for(let j =0;j<Arr2D[i].length;j++){
+//             console.log(Arr2D[i][0] + Arr2D[i][Arr2D[i].length - 1]);
+//     }
+// }
