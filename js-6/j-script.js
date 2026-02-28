2040b74d0630aaebca8d83c4869b93fa321da0fd
ar1kito <sabaabashidze714@gmail.com>
2026-02-28 20:14:31 +0400

    js-6

---
+++ js-6/j-script.js
@@ -0,0 +1,28 @@
+
+// 1
+// function StrFilrt(arr,str){
+//     return arr.filter( check = x => x.includes(str) )
+// }
+
+// let StrArr = ["Lorem, ipsum dolor.","Lorem ipsum dolor sit amet.","amet consectetur, adipisicing elit. Optio, impedit?"]
+// console.log(StrFilrt(StrArr,"Lorem"));
+
+// 2  
+// function RicxArr(arr,num1,num2){
+//     return arr.filter(chek = x => num1 < x && x < num2)
+// }
+// console.log(RicxArr([1,2,3,6,7,8,9,5,5,4.1],3,8));
+
+// 3 
+// function StrChek(arr=[],sityva=""){
+//     return arr.every( x = x => x.includes(sityva))
+// }
+// let winadadeba = ["lorem es pu","lorem shi te stery","chi lamners putyri tolerd lorem"]
+// console.log(StrChek(winadadeba,"lorem"));
+
+// 4
+// function RicxArr(arr,num){
+//     return arr.some( chek => chek > num)
+// }
+// let arr = [1,2,3,6,7,8,9,5,5,4.1]
+// console.log(RicxArr(arr,10));
